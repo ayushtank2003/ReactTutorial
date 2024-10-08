fa0609090a135697a29b9ebd1940a6ad9e927e4c
ayushtank2003 <ayushtaank2003@gmail.com>
2024-10-08 19:11:20 +0530

    props

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import Header from './components/Headerss';
 import MainBar from './components/MainBar';
 import Proptutorial from './components/Proptutorial';
-import  SampleEvent  from './components/SampleEvent';
+// import  SampleEvent  from './components/SampleEvent';
 import StudentResult from './components/StudentResult';
 function App() {
+  const user={
+    id:11,
+    text:"object Data"
+  };
   return (
     <>
     <Header/>
     <p className='appP'>This is for App.js Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non aliquam minima nihil at fugit fugiat qui perspiciatis reprehenderit deleniti ipsa soluta eveniet, aliquid, quas cumque illo repellat! Minima, natus magnam?</p>
     <MainBar/>
-    <Proptutorial welcomemessage="welcome have a nice day" name="john" age={55} />
+    <Proptutorial user={user} welcomemessage="welcome have a nice day" name="john" age={55} />
     {/* <SampleEvent/> */}
     <StudentResult/>
     </>
--- src/components/Proptutorial.js
+++ src/components/Proptutorial.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 const Proptutorial = (props) => {
+    const user=props.user;
   return (
     <>
         <h1>This is Prop tutorial</h1>
         <h4>{props.welcomemessage}</h4>
+        <h5>{props.name}</h5>
+        <h5>{props.age}</h5>
+        <h2>the id is{user.id}</h2>
     </>
     
   )
 }
 
+//default props doesnt discepency with original value but id value is not provided the this is used as default value
+Proptutorial.defaultProps = {
+    age:33
+}
 export default Proptutorial
